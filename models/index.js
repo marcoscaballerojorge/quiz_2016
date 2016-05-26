45b5390a6fb8b01efe0b449c686b0917d465d849
marcoscaballerojorge <marcos.caballero.jorge@alumnos.upm.es>
2016-05-26 18:23:48 +0200

    autor comentario

--- models/index.js
+++ models/index.js
@@ -69,6 +69,10 @@ Quiz.hasMany(Comment);
 User.hasMany(Quiz, {foreignKey: 'AuthorId'});
 Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
 
+// Relacion 1 a N entre User y Quiz:
+User.hasMany(Comment, {foreignKey: 'AuthorId'});
+Comment.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
+
 // Relacion 1-a-1 ente Quiz y Attachment
 Attachment.belongsTo(Quiz);
 Quiz.hasOne(Attachment);
